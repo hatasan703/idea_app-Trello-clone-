5f376220803f42bc9ecf774a36a2cc014641c34e
hatasan703 <h.hatasan09@gmail.com>
2020-03-05 20:45:05 +0900

    migrationファイルを整理

--- db/migrate/20200217094338_add_row_order_to_memos.rb
+++
@@ -1,5 +0,0 @@
-class AddRowOrderToMemos < ActiveRecord::Migration[5.2]
-  def change
-    add_column :memos, :row_order, :integer
-  end
-end
--- db/migrate/20200304105435_add_order_to_memos.rb
+++ db/migrate/20200304105435_add_order_to_memos.rb
@@ -1,6 +1,5 @@
 class AddOrderToMemos < ActiveRecord::Migration[5.2]
   def change
     add_column    :memos, :position, :integer
-    remove_column :memos, :row_order, :integer
   end
 end
