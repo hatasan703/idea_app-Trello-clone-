98c67aa75b6a004471a7216e6d3cb62397e2200f
hatasan703 <h.hatasan09@gmail.com>
2020-03-19 11:22:51 +0900

    テンプレート整理、ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "top#index"
+  devise_scope :user do
+    authenticated :user do
+      root :to => "top#index", as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 
   resources :users
   resources :ideas do  
