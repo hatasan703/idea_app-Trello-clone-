5a862c62aa11579aec2bb8d5d60ba93edbabac25
柳雄大 <yanagi@primal-biz.co.jp>
2020-04-08 18:05:02 +0900

    ニュースクエリ作成機能を実装例外処理はまだ

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,8 @@
 class Idea < ApplicationRecord
+  require 'net/http'
+  require 'uri'
+  require 'json'
+  before_save :create_news_query
   acts_as_list scope: :user
   belongs_to :user
   has_many :memos, ->{ order(position: :asc)  }, dependent: :destroy
@@ -8,4 +12,19 @@ class Idea < ApplicationRecord
 
   scope :sorted, ->{ order(position: :asc)  }
   validates :content, presence: true
+  def create_news_query
+    words = URI.escape("#{self.content}")
+    uri = URI.parse("https://jlp.yahooapis.jp/KeyphraseService/V1/extract?appid=dj00aiZpPWMwNFNqT2g0NGlNRiZzPWNvbnN1bWVyc2VjcmV0Jng9MzI-&sentence=#{words}&output=json")
+    response = Net::HTTP.get_response(uri)
+    hsh =JSON.parse(response.body)
+    news_query = ""
+    count =0
+    hsh.each_key do |k|
+      news_query +="#{k}"
+      count +=1
+      break if count==3
+      news_query +="+"
+    end
+    self.query_word =news_query
+  end
 end
