1dd444497560e63fae54c5cd0299753ea4c37bd9
柳雄大 <yanagi@primal-biz.co.jp>
2020-04-13 17:51:13 +0900

    ideasテーブルのtitleカラムにデフォルト値を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_093736) do
+ActiveRecord::Schema.define(version: 2020_04_13_084634) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content", null: false
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 2020_04_08_093736) do
   end
 
   create_table "ideas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title", null: false
+    t.string "title", default: "", null: false
     t.text "content", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
