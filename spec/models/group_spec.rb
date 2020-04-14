2e1bb5724573e8db970f9d674ad556aa0d72c163
柳雄大 <yanagi@primal-biz.co.jp>
2020-04-14 18:10:20 +0900

    groupモデルに関する基礎的なテストを作成

---
+++ spec/models/group_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Group, type: :model do
+  describe "モデル" do
+    let(:association) do
+       described_class.reflect_on_association(target)
+    end
+    context "アソシエーション" do
+      let(:target){:members}
+      it{expect(association.macro).to eq :has_many}
+
+      let(:target){:users}
+      it{expect(association.macro).to eq :has_many}
+    end
+  end
+  # pending "add some examples to (or delete) #{__FILE__}"
+end
