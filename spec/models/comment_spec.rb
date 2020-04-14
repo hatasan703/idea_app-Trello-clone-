d318c0134a74af5826c994b0c831c02f8ffb1ade
柳雄大 <yanagi@primal-biz.co.jp>
2020-04-14 17:52:49 +0900

    commentモデルのテストを追加

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  describe "モデル" do
+    let(:association) do
+       described_class.reflect_on_association(target)
+    end
+    context "アソシエーション" do
+      let(:target){:user}
+      it{expect(association.macro).to eq :belongs_to}
+
+      let(:target){:idea}
+      it{expect(association.macro).to eq :belongs_to}
+    end
+  end
+  # pending "add some examples to (or delete) #{__FILE__}"
+end
