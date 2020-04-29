c53de14d11c843dda16672bf0673330b74891009
hatasan703 <h.hatasan09@gmail.com>
2020-04-29 19:47:29 +0900

    ユーザーのリンク先をプロフィールに修正、ヘッダーにドロップダウンに実装

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,13 @@
 class ProfilesController < ApplicationController
-  before_action :set_profile,only: [:new,:create,:edit,:update]
+  before_action :set_profile, except: :index
+
   def index
   end
+
   def new
     @user.build_profile
   end
+
   def create
     @user = User.new(profile_params)
     if @user.save
@@ -19,6 +22,7 @@ class ProfilesController < ApplicationController
       redirect_to edit_profile_path
     end
   end
+
   def edit
     @user = User.find(params[:id])
     if @user.profile == nil
@@ -26,11 +30,16 @@ class ProfilesController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
   def set_profile
     @user = User.find(current_user.id)
   end
+
   def profile_params
     params.require(:user).permit(:id,profile_attributes:[:id,:image,:name,:sex,:self_introduction])
   end
+
 end
