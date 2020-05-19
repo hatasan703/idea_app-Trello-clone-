c1a8af3a1e13fba44dc9598ed7c79c8447cb542f
hatasan703 <h.hatasan09@gmail.com>
2020-05-19 21:31:40 +0900

    ユーザのプロフィール閲覧(DM機能)を自分と同じ会社所属ユーザーのみに限定

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -32,23 +32,28 @@ class ProfilesController < ApplicationController
 
   def show
     @user=User.find(params[:id])
-    @currentUserEntry=Entry.where(user_id: current_user.id)
-    @userEntry=Entry.where(user_id: @user.id)
-    if @user.id == current_user.id
-    else
-      @currentUserEntry.each do |cu|
-        @userEntry.each do |u|
-          if cu.room_id == u.room_id then
-            @isRoom = true
-            @roomId = cu.room_id
+    is_colleague = @user.companies & current_user.companies
+    if is_colleague.present?
+      @currentUserEntry=Entry.where(user_id: current_user.id)
+      @userEntry=Entry.where(user_id: @user.id)
+      if @user.id == current_user.id
+      else
+        @currentUserEntry.each do |cu|
+          @userEntry.each do |u|
+            if cu.room_id == u.room_id then
+              @isRoom = true
+              @roomId = cu.room_id
+            end
           end
         end
+        if @isRoom
+        else
+          @room = Room.new
+          @entry = Entry.new
+        end
       end
-      if @isRoom
-      else
-        @room = Room.new
-        @entry = Entry.new
-      end
+    else
+      redirect_to root_path
     end
   end
 
